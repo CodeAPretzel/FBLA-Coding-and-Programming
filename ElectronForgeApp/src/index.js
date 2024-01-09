0743afbf22abbedc32ac6a548223e2ff968662a1
CodeAPretzel <pretzeldevro@gmail.com>
2024-01-08 22:50:17 -0600

    Commit and Addition of Files
    Committed, added, and modified a few files for our production workflow. Not a whole lot, just a few lines of code to progress our building and runtime.

--- ElectronForgeApp/src/index.js
+++ ElectronForgeApp/src/index.js
@@ -11,8 +11,10 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      fullscreen: true,
     },
   });
 
